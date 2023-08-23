60ef7b91c8aceb634fb24d3d2d168fd958da6f18
DIS686 <106627508+maksimd31@users.noreply.github.com>
2023-08-23 19:41:42 +0300

    hm2 working v1

--- Homework/Homework/settings.py
+++ Homework/Homework/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os.path
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-)wlgt08)!lozp49q@b-_7c8k*hlo3wb_sf&c59mw-^rv3m4d)b
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 # Application definition
 
@@ -160,3 +160,6 @@ LOGGING = {
     }
 
 }
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
--- Homework/Homework2app/models.py
+++ Homework/Homework2app/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
+
 # Create your models here.
+class Client(models.Model):
+    name = models.CharField(max_length=100)  # буквы
+    email = models.EmailField()
+    telefone_number = models.IntegerField()
+    adress = models.TextField(blank=True)
+    date = models.DateTimeField(auto_now_add=True)
+
+
+class Products(models.Model):
+    product_name = models.CharField(max_length=150)
+    product_descripsion = models.TextField(blank=True)
+    product_price = models.IntegerField()
+    product_quantiti = models.IntegerField(max_length=100)
+    product_add_date = models.DateTimeField(auto_now=True)
+    product_photo = models.ImageField(upload_to='photos/%y/%m%d/')
+
+
+class Order(models.Model):
+    client = models.ForeignKey(Client, on_delete=models.CASCADE)
+    products = models.ManyToManyField(Products)
+    total_price = models.IntegerField()
+    order_date = models.DateTimeField(auto_now_add=True)
