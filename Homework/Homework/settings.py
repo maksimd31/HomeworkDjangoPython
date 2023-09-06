e0b0219759171dc1258e727d49f767a056b5a0b2
DIS686 <106627508+maksimd31@users.noreply.github.com>
2023-09-06 22:28:58 +0300

    Hm6 version1.3

--- Homework/Homework/settings.py
+++ Homework/Homework/settings.py
@@ -29,7 +29,11 @@ CSRF_COOKIE_SECURE = True
 
 SECRET_KEY = os.getenv('SECRET_KEY')
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = [
+    '127.0.0.1',
+    'ms30.pythonanywhere.com'
+]
+
 INTERNAL_IPS = [
     '127.0.0.1', ]
 # Application definition
@@ -177,7 +181,8 @@ LOGGING = {
 }
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = BASE_DIR / 'static/'
 STATICFILES = []
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
