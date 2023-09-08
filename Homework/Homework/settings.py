4508d3c43053b29777b16b5c9bba9ac1f2ce57f8
DIS686 <106627508+maksimd31@users.noreply.github.com>
2023-09-08 19:30:28 +0300

    Hm6 version 2.1

--- Homework/Homework/settings.py
+++ Homework/Homework/settings.py
@@ -214,11 +214,11 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import os.path
 from pathlib import Path
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-load_dotenv(BASE_DIR / '.env')
+# load_dotenv(BASE_DIR / '.env')
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
