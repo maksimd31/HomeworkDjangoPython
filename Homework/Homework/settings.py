0de30a856ce06b26a21f8da44eba80e5fbf7015d
DIS686 <106627508+maksimd31@users.noreply.github.com>
2023-09-08 20:46:33 +0300

    Hm6 version 2.1

--- Homework/Homework/settings.py
+++ Homework/Homework/settings.py
@@ -214,11 +214,11 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import os.path
 from pathlib import Path
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-load_dotenv(BASE_DIR / '.env')
+# load_dotenv(BASE_DIR / '.env')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -236,6 +236,9 @@ ALLOWED_HOSTS = [
     'ms30.pythonanywhere.com'
 ]
 
+INTERNAL_IPS = [
+    '127.0.0.1',
+]
 # Application definition
 
 INSTALLED_APPS = [
@@ -287,7 +290,7 @@ WSGI_APPLICATION = 'Homework.wsgi.application'
 #         'ENGINE': 'django.db.backends.sqlite3',
 #         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
-# }
+# # }
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
