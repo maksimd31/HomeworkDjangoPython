f360f62f084602079963b3eca3fd4abdc46ad015
macbook13 <maksimdis31@gmail.com>
2023-09-10 22:47:15 +0300

    hm6 version 2.4

--- Homework/Homework/wsgi.py
+++ Homework/Homework/wsgi.py
@@ -8,9 +8,12 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Homework.settings')
 
 application = get_wsgi_application()
+
+sys.path.append('/home/ms30/.local/lib/python3.10/site-packages/dotenv/__init__.py')
